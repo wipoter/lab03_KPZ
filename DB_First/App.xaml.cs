b3628b4c4dfbab1b353d1cd6a9f724523918ae23
Dmytro Sushchyk <75736905+wipoter@users.noreply.github.com>
2022-11-04 16:26:12 +0200

    Add files via upload

---
+++ DB_First/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DB_First
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+        }
+    }
+}
